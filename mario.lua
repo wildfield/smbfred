47478070b78857b3b928d30a7d9fe1db74930afe
Dmitry Kasianchuk <wildfield@live.ru>
2016-08-22 19:24:22 +0300

    Added tile drawing routine

--- mario.lua
+++ mario.lua
@@ -4,10 +4,32 @@ marioHeight = 32
 -- 0x0087/B
 -- 0x00CF-0x00D3
 
+-- Tile positions
+local tiles_start_addr = 0x0500
+local tiles_final_addr = 0x069F
+local tiles_count = tiles_final_addr - tiles_start_addr + 1
+
+function tile2px(tilenum, screenpos)
+  if tilenum < 0 or tilenum >= 0x1A0 then
+    return false;
+  end;
+
+  px      = (math.fmod(tilenum, 0x10) + math.floor(tilenum / 0xD0) * 0x10) * 0x10;
+  px      = px - math.fmod(screenpos, 0x200);
+--  text(8,  8, string.format("PX[%4d] OF[%4X]", px, offset));
+  if px < 0 then
+    px    = px + 0x200;
+  end;
+
+  py      = math.floor(math.fmod(tilenum, 0xD0) / 0x10);
+  returnval = {x = px, y = py};
+  return returnval;
+end;
+
 function everyframe()
        marioX = memory.readbyte(0x3AD)
        marioTrueX = memory.readbyte(0x0086)
-       screenOffsetRel = memory.readbyte(0x071D)
+       screenOffsetRel = memory.readbyte(0x071C)
        screenOffsetMult = memory.readbyte(0x071A)
        screenOffset = screenOffsetRel + screenOffsetMult * 256
 
@@ -39,6 +61,15 @@ function everyframe()
        		gui.drawbox(enemyX, enemyY + 8, enemyX + 16, enemyY + 24, "clear", "red")
        	end
        end
+
+    for i = 1, tiles_count do
+      tile_num = i
+      tile_addr = tiles_start_addr + i - 1
+      tile = memory.readbyte(tile_num)
+      if tile ~= 0 then
+        tile_px = tile2px(tile, screenOffset)
+        gui.drawbox(tile_px['x'], tile_px['y'], tile_px['x'] + 16, tile_px['y'] + 16, "clear", "black")
+    end
 end
 
 emu.registerafter(everyframe)
