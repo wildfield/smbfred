faa65cabd5acfe9205f21d69e2cdbb594ae8bccd
Wildfield <wildfield@live.ru>
2016-08-22 21:28:50 +0300

    Finished tiles highlighting

--- mario.lua
+++ mario.lua
@@ -6,10 +6,15 @@ marioHeight = 32
 
 -- Tile positions
 local tiles_start_addr = 0x0500
-local tiles_final_addr = 0x05cf
+local tiles_half_addr = 0x05cf
+local tiles_final_addr = 0x069F
 local tiles_count = tiles_final_addr - tiles_start_addr + 1
 
-function tile2px(tilenum, screenpos)
+-- Converts tile number to pixel number
+-- Each tile is drawn on page of 208 tiles, 32 columns
+-- Tiles are drawn on two pages: 0x0500 to 0x05cf and 0x05cf to 0x069F
+-- Pages are swapped while mario is running, so we are seeing page 1, page 2, page 1, etc...
+function tile2px(tilenum, screenpos, current_page)
   if tilenum < 0 or tilenum >= 0x1A0 then
     return false;
   end;
@@ -17,8 +22,13 @@ function tile2px(tilenum, screenpos)
   page_size = 208
   page = math.floor(tilenum / page_size)
   page_x = math.fmod(tilenum, page_size) 
-  tile_x = math.fmod(page_x, 0x10) * 0x10 + page * 0x100 - screenpos
-  py = math.floor(math.fmod(tilenum, page_size) / 0x10) * 0x10 + 20
+  tile_x = math.fmod(page_x, 0x10) * 0x10 + page * 0x100 + math.floor(current_page / 2) * 0x200 - screenpos
+
+  if tilenum < tiles_count / 2 and math.fmod(current_page, 2) == 1 then
+  	tile_x = tile_x + 0x200
+  end
+
+  py = math.floor(math.fmod(tilenum, page_size) / 0x10) * 0x10 + 0x20
   -- print(tilenum, screenpos, px)
 
   returnval = {x = tile_x, y = py};
@@ -61,12 +71,14 @@ function everyframe()
        	end
        end
 
+    current_page = screenOffsetMult
+
     for i = 1, tiles_count do
       tile_num = i
       tile_addr = tiles_start_addr + i - 1
       tile = memory.readbyte(tile_addr)
       if tile ~= 0 then
-        tile_px = tile2px(tile_num - 1, screenOffset)
+        tile_px = tile2px(tile_num - 1, screenOffset, screenOffsetMult)
         -- print(tile_num, tile_addr, tile, tile_px)
         if tile_px['x'] >= 0 and tile_px['y'] >= 0 then
           gui.drawbox(tile_px['x'], tile_px['y'], tile_px['x'] + 16, tile_px['y'] + 16, "clear", "white")
